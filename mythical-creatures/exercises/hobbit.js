97f13e0cfaa6251768b9781ebd910482544d7e0c
Jocelle Bautista <jcllebautista@gmail.com>
2022-10-17 19:37:06 -0600

    Complete Hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -3,11 +3,24 @@ class Hobbit {
         this.name = shirePeople.name
         this.age = 0
         this.adult = false
+        this.old = false
+        this.hasRing = false
     }
     celebrateBirthday() {
         this.age++
-        if(this.age > 32) {
-            return this.adult
+        if (this.age > 32) {
+            this.old = false
+            this.adult = true
+        } if (this.age > 100) { //why is it that if i put "else if," it returns false rather, it passes with just an "if"?
+            return this.old = true
+        }
+    }
+    getRing() {
+        if(this.name === 'Frodo') {
+            this.hasRing = true
+            return 'Here is the ring!'
+        } else {
+            return 'You can\'t have it!'
         }
     }
 }
